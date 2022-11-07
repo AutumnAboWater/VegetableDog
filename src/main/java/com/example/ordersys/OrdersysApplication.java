4e2597162fd01e3c643859da7d63341a97684175
11961 <1196105971@qq.com>
2022-11-07 18:24:30 +0800

    菜狗点餐系统

---
+++ src/main/java/com/example/ordersys/OrdersysApplication.java
@@ -0,0 +1,17 @@
+package com.example.ordersys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 说明：前台登录地址:<a href="http://localhost:8080/index.html">...</a>
+ * 后台管理登录地址:<a href="http://localhost:8080/admin.html">...</a>
+ */
+@SpringBootApplication
+public class OrdersysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdersysApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/ordersys/config/AppFinal.java
@@ -0,0 +1,11 @@
+package com.example.ordersys.config;
+
+public class AppFinal {
+    /**
+     * 当浏览器第一次访问服务器时，服务器创建一个session对象(该
+     * 对象有一个唯一的id,一般称之为sessionId),服务器会将sessionId
+     * 以cookie的方式发送给浏览器。
+     */
+    //用户的session
+    public static final String USERINFO_SESSION_KEY = "USERINFO_SESSION_KEY";
+}
